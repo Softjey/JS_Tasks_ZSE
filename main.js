bc1100973ca94f6980d1bfcc3afe702b29579621
Softjey <misivsvatoslav@gmail.com>
2023-11-16 10:08:24 +0100

    create main.js, buttons already create from JS, add code for z1-z10

---
+++ main.js
@@ -0,0 +1,45 @@
+function WinOpen(task, code = false, height = 800, width = 800, top = 100, left = 100) {
+  if (code) {
+    window.open(
+      `z${task}/z${task}_mysi_kod.html`,
+      `okienko_z${task}_kod`,
+      `toolbar=no, directories=no, menubar=no, height=${height}, width=${width}, top=${top}, left=${left}`);
+
+    return;
+  }
+
+  window.open(
+    `z${task}/z${task}_mysi.html`,
+    `okienko_z${task}`,
+    `toolbar=no, directories=no, menubar=no, height=${height}, width=${width}, top=${top}, left=${left}`);
+}
+
+function createTaskButton(index) {
+  const button = document.createElement('input');
+  button.type = 'button';
+
+  button.name = `zadanie${index}`;
+  button.value = `zadanie${index}-Mysiv`;
+  button.onclick = () => { WinOpen(index) };
+
+  return button;
+}
+
+function createCodeButton(index) {
+  const button = document.createElement('input');
+  button.type = 'button';
+
+  button.name = `zadanie${index}-kod`;
+  button.value = `zadanie${index}-Mysiv-kod`;
+  button.onclick = () => { WinOpen(index, true) };
+
+  return button;
+}
+
+const form = document.querySelector('.zad-buttons');
+const ZAD_COUNT = 30;
+
+for (let i = 1; i <= ZAD_COUNT; i++) {
+  form.appendChild(createTaskButton(i));
+  form.appendChild(createCodeButton(i));
+}
